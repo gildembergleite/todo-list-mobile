38fb2e2dbe602670a9d2ae22fe931a0a63693b08
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-04 14:46:22 -0300

    style: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import styles from './styles'
 
 export default function Header() {
   return (
-    <View>
-      <Text>to</Text>
-      <Text>do</Text>
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Text style={styles.to}>to</Text>
+        <Text style={styles.do}>do</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Adicione uma nova tarefa'
+          placeholderTextColor={'#808080'}
+        />
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.textAddButton}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,62 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-  
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0D0D0D',
+    padding: 40,
+    position: 'relative'
+  },
+  logo: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 30,
+  },
+  to: {
+    color: '#4EA8DE',
+    fontSize: 42,
+    fontWeight: '900',
+  },
+  do: {
+    color: '#5E60CE',
+    fontSize: 42,
+    fontWeight: '900',
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    gap: 4,
+  },
+  textInput: {
+    backgroundColor: '#262626',
+    display: 'flex',
+    flex: 1,
+    fontSize: 16,
+    padding: 16,
+    borderRadius: 6,
+    marginBottom: -70,
+  },
+  addButton: {
+    backgroundColor: '#1E6F9F',
+    width: 60,
+    height: 60,
+    borderRadius: 6,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: -70,
+  },
+  textAddButton: {
+    color: '#fff',
+    fontSize: 20,
+  }
 })
+
+export default styles
