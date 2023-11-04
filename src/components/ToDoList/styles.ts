156ec6657adca67019ab2d17390550793c7ffd38
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-04 19:32:37 -0300

    fix: correction in scroll view

--- src/components/ToDoList/styles.ts
+++ src/components/ToDoList/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    flex: 1,
     width: '100%',
     paddingHorizontal: 24,
   },
